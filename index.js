6bb81cdc5b266a60d0bd7f6919417112c04e09b2
antoshagagarin <antoshagagarin1@gmail.com>
2023-07-11 21:32:53 +0300

    some changes

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ const arr = "qwertyuiop[]asdfghjkl;zxcvbnm,./{}<>?:()iuercvbiuretcvbn".split(
 const p = document.querySelector("p");
 
 function random(max) {
-  min = 0;
-  max = Math.floor(max);
-  const res = Math.floor(Math.random() * (max - min + 1)) + min;
+  let min = 0;
+  let max = Math.floor(max);
+  let res = Math.floor(Math.random() * (max - min + 1)) + min;
+  if (arr[res] === p.textContent) {
+    res = Math.floor(Math.random() * (max - min + 1)) + min;
+  }
   p.textContent = arr[res];
 }
 
 random(arr.length - 1);
 
 window.addEventListener("keydown", (e) => {
-  if (e.key == p.textContent) {
+  if (e.key.toLowerCase() == p.textContent) {
     random(arr.length - 1);
+    p.style.color = "#aaa";
+  } else if (e.key !== p.textContent && e.key !== "Shift") {
+    p.style.color = "tomato";
   }
 });
