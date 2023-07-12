fab03dc058796bc78e1117872f45dcf6200434b3
antoshagagarin <antoshagagarin1@gmail.com>
2023-07-12 14:34:29 +0300

    some changes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const arr = "qwertyuiop[]asdfghjkl;xcvbnm,./{}<>?:()iuercvbiuretcvbn".split("");
+const arr = "qwertyuiop[]asdfghjkl;xcvbnm,./{}<>?:()iuercvbiuretcvbn-+=_"
+  .toUpperCase()
+  .split("");
 const p = document.querySelector("p");
 
 function random(digit) {
@@ -8,13 +10,14 @@ function random(digit) {
   if (arr[res] === p.textContent) {
     res = Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
   p.textContent = arr[res];
 }
 
 random(arr.length - 1);
 
 window.addEventListener("keydown", (e) => {
-  if (e.key.toLowerCase() == p.textContent) {
+  if (e.key.toUpperCase() == p.textContent) {
     random(arr.length - 1);
     p.style.color = "#aaa";
   } else if (e.key !== p.textContent && e.key !== "Shift") {
