2e83b83f7f49776d30e5dc3f06172e5e6f0dba65
antoshagagarin <antoshagagarin1@gmail.com>
2023-07-12 23:07:20 +0300

    some changes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+//создаем массив тех симолов, что будут в тренажере
 const arr = "qwertyuiop[]asdfghjkl;xcvbnm,./{}<>?:()iuercvbiuretcvbn-+=_".split(
   ""
 );
 const p = document.querySelector("p");
-
+//рандомный рендер символа
 function random(digit) {
   let min = 0;
   let max = Math.floor(digit);
@@ -15,7 +16,7 @@ function random(digit) {
 }
 
 random(arr.length - 1);
-
+//слушатель по нажатию на клавиатуру
 window.addEventListener("keydown", (e) => {
   if (e.key == p.textContent) {
     random(arr.length - 1);
@@ -23,7 +24,7 @@ window.addEventListener("keydown", (e) => {
     p.classList.add("errorAnim");
   }
 });
-
+//слушатель чтобы снять класс анимации после завершения анимации
 p.addEventListener("animationend", () => {
   p.classList.remove("errorAnim");
 });
